db531d7da4f90d04486f54d758c972568d9e7c18
Marcelo Leite <marceloleite2604@gmail.com>
2021-12-26 11:41:49 -0300

    Add debug on rising and falling piercing line patterns

--- src/main/java/com/github/marceloleite2604/cryptotrader/service/pattern/links/RisingPiercingLinePatternChecker.java
+++ src/main/java/com/github/marceloleite2604/cryptotrader/service/pattern/links/RisingPiercingLinePatternChecker.java
@@ -5,12 +5,14 @@ import com.github.marceloleite2604.cryptotrader.model.candles.CandleDirection;
 import com.github.marceloleite2604.cryptotrader.model.candles.CandleProportion;
 import com.github.marceloleite2604.cryptotrader.model.pattern.PatternType;
 import com.github.marceloleite2604.cryptotrader.service.pattern.PatternCheckContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class RisingPiercingLinePatternChecker extends AbstractPatternChecker {
 
   private static final List<CandleProportion> VALID_CANDLE_SIZE_CATEGORIES = List.of(
@@ -48,6 +50,9 @@ public class RisingPiercingLinePatternChecker extends AbstractPatternChecker {
     if (CandleDirection.ASCENDING.equals(secondCandle.getDirection())) {
       return Optional.empty();
     }
+
+    log.debug("First candle: {}", firstCandle);
+    log.debug("Second candle: {}", secondCandle);
 
     return Optional.of(firstCandle);
   }
